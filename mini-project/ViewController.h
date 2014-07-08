923d9375ccd142bcfcfc18657284335feca8fcad
Mengdi Lin <mengdilin95@gmail.com>
2014-07-08 10:07:51 -0700

    Initial Commit

---
+++ mini-project/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mini-project
+//
+//  Created by Mengdi Lin on 7/8/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ mini-projectTests/mini_projectTests.m
@@ -0,0 +1,34 @@
+//
+//  mini_projectTests.m
+//  mini-projectTests
+//
+//  Created by Mengdi Lin on 7/8/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface mini_projectTests : XCTestCase
+
+@end
+
+@implementation mini_projectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
