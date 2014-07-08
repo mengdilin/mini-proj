6f385e30a4e1c076aa610c7451f1bab46905913e
Mengdi Lin <mengdilin95@gmail.com>
2014-07-08 10:38:56 -0700

    added what's open and restaurant placeholder

---
+++ mini-project/Restaurant.m
@@ -0,0 +1,13 @@
+//
+//  Restaurant.m
+//  mini-project
+//
+//  Created by Mengdi Lin on 7/8/14.
+//  Copyright (c) 2014 Mengdi Lin. All rights reserved.
+//
+
+#import "Restaurant.h"
+
+@implementation Restaurant
+
+@end
